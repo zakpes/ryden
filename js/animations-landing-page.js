531472149fd8e36e04f547cd463c4a86e66f557a
zakpes <zarkopesic@mail.com>
2016-04-09 02:39:55 +0200

    Initial commit

---
+++ js/animations-landing-page.js
@@ -0,0 +1,146 @@
+$(function main() {
+
+
+    /* animates grey and green car moving and battery charging on scroll */
+
+    var $carGrey = $(".car-grey");
+    var $carGreen = $(".car-green");
+    var $greyPath = $(".car-grey-path");
+    var $greenPath = $(".car-green-path");
+    var $miles = $(".miles");
+    var $fillGreen = $(".fill-green");
+
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 1600) {
+            $carGrey.addClass("car-grey-move");
+            $carGreen.addClass("car-green-move");
+            $greyPath.addClass("grey-path-move");
+            $greenPath.addClass("green-path-move");
+            $miles.addClass("miles-move");
+            $fillGreen.addClass("fill-green-ani");
+        };
+        /*$(".miles").css("margin-left", "70px").delay(slow);*/
+
+    });
+
+
+    /* car movement animation on mouseenter
+    $(".car-container").one("mouseenter", function() {
+        carGrey.removeClass("car-grey-move").width();
+        carGreen.removeClass("car-green-move").width();
+        greyPath.removeClass("grey-path-move").width();
+        greenPath.removeClass("green-path-move").width();
+        miles.removeClass("miles-move").width();
+        carGrey.addClass("car-grey-move2");
+        carGreen.addClass("car-green-move2");
+        greyPath.addClass("grey-path-move2");
+        greenPath.addClass("green-path-move2");
+        miles.addClass("miles-move2");
+    });
+
+    battery chargin animation on mouseenter
+    $(".battery-fill-container").one("mouseenter", function() {
+        fillGreen.removeClass("fill-green-ani").width();
+        fillGreen.addClass("fill-green-ani2");
+
+    });
+    */
+
+    /* animates recycle icon*/
+    $(".recycle-animate").mouseenter(function() {
+        $(".icon-recycle").addClass("recycle-spin");
+    });
+
+    $(".recycle-animate").mouseleave(function() {
+        $(".icon-recycle").removeClass("recycle-spin");
+    });
+
+    /* smooth scrolling between sections
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+        }
+    });*/
+
+    smoothScroll(1000);
+
+    function smoothScroll(duration) {
+        $("a[href*=#]:not([href*=#][data-toggle='tab'])").click(function(event) {
+
+            var target = $( $(this).attr("href") );
+
+            if (target.length) {
+                event.preventDefault();
+                $("html, body").animate({
+                    scrollTop: target.offset().top
+                }, duration);
+            }
+        });
+    };
+/*
+     technology section text changer
+
+    var $rel = $(".text-container-reliability");
+    var $safe = $(".text-container-safety");
+    var $cost = $(".text-container-cost");
+    var $labCost = $(".label-cost");
+    var $labSafe = $(".label-safety");
+    var $labRel = $(".label-reliability");
+
+    $labRel.click(function() {
+        $rel.addClass("active").fadeIn(1000);
+        $safe.removeClass("active").fadeOut(1000);
+        $cost.removeClass("active").fadeOut(1000);
+
+        $labCost.removeClass("active");
+        $labSafe.removeClass("active");
+        $labRel.addClass("active");
+    });
+
+    $labSafe.click(function() {
+        $cost.removeClass("active").fadeOut(1000);
+        $rel.removeClass("active").fadeOut(1000);
+        $safe.addClass("active").fadeIn(1000);
+
+        $labRel.removeClass("active");
+        $labCost.removeClass("active");
+        $labSafe.addClass("active");
+    });
+
+    $labCost.click(function() {
+        $rel.removeClass("active").fadeOut(1000);
+        $safe.removeClass("active").fadeOut(1000);
+        $cost.addClass("active").fadeIn(1000);
+
+        $labRel.removeClass("active");
+        $labSafe.removeClass("active");
+        $labCost.addClass("active");
+    });*/
+
+    var label = ".label-tech";
+    var textToHide = ".text-container";
+    $(".text-container-reliability, .text-container-safety").hide();// hide containers initially
+
+    $(label).click(function() {
+
+        $(label + ".active").removeClass("active");
+        $(this).addClass("active");
+
+        var $textToShow = $(this).attr("rel");
+
+        $(textToHide + ".active").removeClass("active").fadeOut(500, function() {
+            $("#" + $textToShow).fadeIn(500).addClass("active");
+        });
+    })
+
+
+
+});
---
+++ js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+        $(".navbar-toggle").click(function() {
+            $(".collapse.navbar-collapse").slideToggle(500);
+        });
+        $(".search-icon-pwj li span").click(function() {
+            $(".pwj-seach-box").slideToggle(500);
+        });
+    });
+     $( "#mouse-hover-js" ).hover(function() {
+        $("#added-hover-opening").addClass( "open" );}, function() {$("#added-hover-opening").removeClass( "open" );
+    });
+     $( "#mouse-hover-js2" ).hover(function() {
+        $("#added-hover-opening2").addClass( "open" );}, function() {$("#added-hover-opening2").removeClass( "open" );
+    });
+
+$(document).ready(function() {
+    var pathArray = window.location.pathname.split( '/' );
+    var NameUrl = pathArray[pathArray.length-1];
+    if((NameUrl=="technology.html")||(NameUrl=="innovation.html")||(NameUrl=="benefits.html")){document.getElementsByClassName("opened-li")[1].style.color="#666666";}
+    if((NameUrl=="company.html")||(NameUrl=="research.html")||(NameUrl=="partnership.html")){document.getElementsByClassName("opened-li")[0].style.color="#666666";}
+    if(NameUrl=="media.html"){document.getElementById("media").style.color="#666666";}
+    if(NameUrl=="contacts.html"){document.getElementById("contact").style.color="#666666";}
+});
+
+/*Company page*/
+    var usa = 1;
+    $( "#photo1" ).click(function() {
+        $("#redline1").css("display", "block");$("#redline2").css("display", "none");$("#redline3").css("display", "none");
+        $("#righttextcontent2").css("display", "none");$("#righttextcontent3").css("display", "none");
+        $( "#righttextcontent1" ).fadeIn("slow", function() {
+            $("#photo1").css("opacity", "1");$("#photo2").css("opacity", "0.4");$("#photo3").css("opacity", "0.4");
+            usa = 1;
+    });});
+
+    $( "#photo2" ).click(function() {
+            $("#righttextcontent1").css("display", "none");$("#righttextcontent3").css("display", "none");
+            $("#redline2").css("display", "block");$("#redline1").css("display", "none");$("#redline3").css("display", "none");
+            $( "#righttextcontent2" ).fadeIn("slow", function() {
+                $("#photo2").css("opacity", "1");$("#photo1").css("opacity", "0.4");$("#photo3").css("opacity", "0.4");
+                
+                usa = 2;
+                });});
+
+    $( "#photo3" ).click(function() {
+        $("#redline3").css("display", "block");$("#redline1").css("display", "none");$("#redline2").css("display", "none");
+        $("#righttextcontent1").css("display", "none");$("#righttextcontent2").css("display", "none");
+        $( "#righttextcontent3" ).fadeIn("slow", function() {
+            $("#photo3").css("opacity", "1");$("#photo1").css("opacity", "0.4");$("#photo2").css("opacity", "0.4");
+            
+            usa = 3;
+    });});
+
+    $( "#photo3" ).hover(function() {$("#photo3").css("opacity", "1");}, function() {if(usa!=3){$("#photo3").css("opacity", "0.5");}});
+    $( "#photo2" ).hover(function() {$("#photo2").css("opacity", "1");}, function() {if(usa!=2){$("#photo2").css("opacity", "0.5");}});
+    $( "#photo1" ).hover(function() {$("#photo1").css("opacity", "1");}, function() {if(usa!=1){$("#photo1").css("opacity", "0.5");}});
+
+
+/* ExternalLinks which opening is in new window*/
+function externalLinks() {
+    links = document.getElementsByTagName("a");
+    for (i=0; i<links.length; i++) {
+      link = links[i];
+      if (link.getAttribute("href") && link.getAttribute("rel") == "external")
+      link.target = "_blank";
+    }
+}
+window.onload = externalLinks;
+/*End ExternalLinks*/
